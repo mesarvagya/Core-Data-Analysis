9f61d513129ae3202d65a5af4347672a9f8d1841
Sarvagya Pant <sarvagya.pant@gmail.com>
2014-05-05 23:11:53 +0545

    Week 3 Updated Code with PLOTS

--- Week3/week3.py
+++ Week3/week3.py
@@ -7,6 +7,13 @@ def linear_reg(filename, col1, col2, precision):
 	rho = round(np.sum((y-mean_y)*(x-mean_x)/len(y))/(std_x*std_y), precision)
 	a = round(rho * std_y / std_x, precision)
 	b = round(mean_y - a * mean_x, precision)
+	xranges = np.linspace(min(x),max(x),1000)
+	yvalues = a * xranges + b
+	# print xranges, yvalues
+	plt.plot(x,y,'r.',xranges,yvalues)
+	plt.axis([min(x)-0.5,max(x)+0.5,min(y)-0.5,max(y)+0.5])
+	plt.title("Scatter Plot and Linear Regression Line")
+	plt.show()
 	return "The Value of a is %s b is %s and determinacy coeffecient is %s" %(a, b, round(rho**2,precision))
 
 print linear_reg("dataset_389_1.txt",col1=2, col2=3, precision=6)
